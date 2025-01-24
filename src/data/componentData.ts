c72a223a2536f5921ec4a5adc317dfebecfb9215
Gerald Lekara <117988041+gerald023@users.noreply.github.com>
2025-01-24 17:27:54 +0200

    added the infosection

--- src/data/componentData.ts
+++ src/data/componentData.ts
@@ -1,24 +1,29 @@
-import icon1 from '../assets/images/analize_crime.png';
+import analize_data from '../assets/images/analize_crime.png';
+import identify_trends from '../assets/images/Data-trends.png';
+import understand_cause from '../assets/images/understanding-cause.png';
+import take_action from '../assets/images/take-action.png';
+
+
 
 export const IconCateData = [
     {
         title: 'Analyze Crime Data',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, architecto?',
-        image: icon1,
+        image: analize_data,
     },
     {
         title: 'Identify Trends',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, architecto?',
-        image: icon1,
+        image: identify_trends,
     },
     {
         title: 'Understand Cause',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, architecto?',
-        image: icon1,
+        image: understand_cause,
     },
     {
         title: 'Take Action',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, architecto?',
-        image: icon1,
+        image: take_action,
     }
 ]
